b677ee922382ff4579010750f1e259f52e664e2d
Osvaldo Leon <osleon.dev@gmail.com>
2022-08-15 18:28:50 -0500

    Add bootstrap styles to project views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ApplicationRecord
+    validates_presence_of :name
+    validates_presence_of :category
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.start_date = Time.now
+    end
 end
